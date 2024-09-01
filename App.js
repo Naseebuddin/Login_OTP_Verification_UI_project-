68e81b5b4d5a6113664a756fe5fd2066087e7a30
Naseebuddin <nzk.7582@gmail.com>
2024-09-01 17:33:29 +0530

    first commit ui screen

---
+++ App.js
@@ -0,0 +1,13 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Routes from './src/navigations/Routes'
+
+const App = () => {
+  return (
+   <Routes/>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
---
+++ src/components/ButtonComp.js
@@ -0,0 +1,29 @@
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React from 'react';
+import colors from '../styles/colors';
+import {moderateScale, textScale, width} from '../styles/responsiveSize';
+
+const ButtonComp = ({buttonText, onPress,width}) => {
+  return (
+    <TouchableOpacity onPress={onPress} style={{...styles.btnStyle,width:width}}>
+      <Text style={styles.textStyle}>{buttonText}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default ButtonComp;
+
+const styles = StyleSheet.create({
+  btnStyle: {
+    backgroundColor: colors.btnColor,
+    height: moderateScale(72),
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textStyle: {
+    fontSize: textScale(18),
+    fontWeight: '600',
+    color: colors.white,
+  },
+});
